54944b988d1231a363d76b3f0c0de50bd8e78ed8
srinigirish <g.srinivasa@bigpond.com>
2020-06-07 13:41:07 +1000

    Add files via upload
    This script will locate the AzureAD PS module from the powershell gallery and installs it at C:\Program Files\WindowsPowerShell\Modules\AzureAD

---
+++ FindInstallAzureAD.ps1
@@ -0,0 +1,5 @@
+﻿
+#This script will locate  and AzureAD module in PS Gallery at https://powershellgallery.com 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Find-Module -Name AzureAD
+Install-Module -Name AzureAD
